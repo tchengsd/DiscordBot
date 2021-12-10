fbe18fffcfba69647fda0d66aa0cdf1a7781cc43
league <league@imac-14.attlocal.net>
2021-12-09 18:25:39 -0800

    Discord Bot Tests (IP)

---
+++ src/DiscordBotTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+public class DiscordBotTest {
+	DiscordBot fakeBot = new DiscordBot("","","");
+	@Test
+	void itShouldSendWhoAmI() {
+		
+	}
+	@Test
+	void itShouldEnterRPS() {
+		
+	}
+}
